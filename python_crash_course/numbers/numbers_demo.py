e282f61b6a2e8b7e0489f05b7d775f6511395e2c
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-03-19 16:35:53 -0400

    python operators

--- python_crash_course/numbers/numbers_demo.py
+++ python_crash_course/numbers/numbers_demo.py
@@ -1,3 +1,5 @@
+# INTEGERS AND FLOATS
+
 # Float has decimal points, integers are whole numbers (negative or positive)
 # floats are bound by a limit of precision. 
 num = 12
@@ -11,3 +13,41 @@ print(type(num))
 num = 6.66
 print(num)
 print(type(num))
+
+# below is a valid number as well
+num = 1_000_000
+print(num)
+print(type(num))
+
+
+# BASIC OPERATORS
+
+num = 1 + 4
+print(num)
+num = 7 - 2
+print(num)
+num = 5 - 2.5
+print(num)
+num = 45 * 45
+print(num)
+num = 5 / 2
+print(num)
+num = 10 / 3.6
+print(num)
+# Order of operations 
+num = 6 * 3 - 10
+print(num)
+num = 6 * (3 - 10)
+print(num)
+
+# Exponentiation
+num = 4 ** 3
+print(num)
+# Integer Division (always rounds down)
+num = 10 / 3
+print(num)
+num = 10 // 3
+print(num)
+# Modulo (Remainder)
+num = 38 % 10
+print(num)
