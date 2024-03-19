31ee80c2504e482b56ae96f9e33a17c81c4dec8e
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-03-19 16:24:08 -0400

    numbers basics in python

---
+++ python_crash_course/numbers/numbers_demo.py
@@ -0,0 +1,13 @@
+# Float has decimal points, integers are whole numbers (negative or positive)
+# floats are bound by a limit of precision. 
+num = 12
+print(num)
+print(type(num))
+
+num = -15
+print(num)
+print(type(num))
+
+num = 6.66
+print(num)
+print(type(num))
