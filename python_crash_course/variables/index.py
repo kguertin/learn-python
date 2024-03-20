11f33a187efa8d4c6ac8f04893e795a02906a414
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-03-20 17:36:08 -0400

    assigning variables

---
+++ python_crash_course/variables/index.py
@@ -0,0 +1,18 @@
+# Intro to Variables
+age = 104
+print(age)
+age + 1
+print(age + 1)
+print(age)
+age = age + 1
+print(age)
+
+score = 500
+print(score * 3)
+score = score * 3
+print(score)
+score = 0
+print(score)
+
+score = 999
+print(score * age)
