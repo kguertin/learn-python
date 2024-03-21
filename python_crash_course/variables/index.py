7755e4c8bf38640e6ed48b71e85cf28231e978f0
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-03-21 10:23:34 -0400

    assignment operators

--- python_crash_course/variables/index.py
+++ python_crash_course/variables/index.py
@@ -16,3 +16,18 @@ print(score)
 
 score = 999
 print(score * age)
+
+# Assignment Operators
+prize_money = 500
+prize_money += 50
+print(prize_money)
+prize_money -= 500
+print(prize_money)
+prize_money *= 10
+print(prize_money)
+prize_money /= 2
+print(prize_money)
+
+# Often used for counters
+site_visitors = 200
+site_visitors += 1
