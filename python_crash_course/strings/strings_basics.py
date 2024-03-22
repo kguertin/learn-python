da76323246c74276db2776d0cb35153244ce7ff3
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-03-22 11:31:42 -0400

    string basics

--- python_crash_course/strings/strings_basics.py
+++ python_crash_course/strings/strings_basics.py
@@ -12,3 +12,27 @@ variable = 48
 print(type(variable))
 variable = "string"
 print(type(variable))
+
+# String Operators - No type coercion?
+opp = "hello" + "World"
+print(opp)
+
+first_name = "Kyle"
+last_name = "Lowry"
+full_name = first_name + " " + last_name
+print(full_name)
+num = "4" + "5"
+print(num)
+
+print("ha" * 19)
+
+# String Indexing
+test = "hello"
+print(test[0])
+print(test[4])
+
+print(full_name[-1])
+
+# None type - Python's version of null
+user = None
+print(type(user))
