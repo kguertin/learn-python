4f20765308e9456871174edbc85805a8065992b2
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-03-22 09:54:22 -0400

    string basics

---
+++ python_crash_course/strings/strings_basics.py
@@ -0,0 +1,14 @@
+# Strings in Python Basics
+string = "Hello World!"
+print(string)
+string = 'hello world?'
+print(string)
+print(type(string))
+string = "You can't use single strings within a string using single quotes"
+print(string)
+
+# In python we can change the types of variables
+variable = 48
+print(type(variable))
+variable = "string"
+print(type(variable))
