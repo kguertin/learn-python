a73377dd18ccaa2c1c0a26cc02944d27e3084114
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-03-27 10:31:54 -0400

    more utility methods for strings

--- python_crash_course/strings/methods.py
+++ python_crash_course/strings/methods.py
@@ -16,3 +16,22 @@ name = ".,.,.,.,,,....Kevin,,,,,,...,,."
 print(name.strip(",."))
 print(name.rstrip(",."))
 print(name.lstrip(",."))
+
+# Replace
+team = "Detroit Raptors"
+print(team.replace("Detroit", "Toronto"))
+prices = "$1.99 $2.99 $3.99"
+print(prices.replace("$", ""))
+print(prices.replace("$", "", 1))
+
+# Find and Index
+print(prices.find("$"))
+print(prices.find("9"))
+print(prices.find("$", 1))
+print(prices.find("$", 7, 9))
+print(prices.count("$"))
+print(prices.count("9", 3, 7))
+
+# Chaining Methods
+email = "         TODD@gmail.com             "
+print(email.lower().strip())
