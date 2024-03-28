9089164d7c9943d584f9fb3955429005a387f288
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-03-28 10:42:43 -0400

    boolean values in python

---
+++ python_crash_course/booleans/booleans.py
@@ -0,0 +1,39 @@
+# Booleans
+
+# Comparison Operators 
+print(10 > 3)
+print(3 > 10)
+age = 19
+print(age > 21)
+print(age < 21)
+print(age >= 21)
+print(21 >= 21)
+print(age <= 10)
+
+# Equality Operators 
+print(10 == 10)
+print(10 == 9)
+print(10 != 10)
+print(10 != 9)
+print("a" == "a")
+print("A" == "a")
+print("A" != "a")
+print(3.5 == 3.5)
+print(3.5 != 10.1)
+print(3.0 == 3)
+
+# Truthyness and Falseyness
+# Similar to how this is divided in JavaScript
+print(bool(0))
+print(bool(1))
+print(bool(""))
+print(bool("Kevin"))
+print(bool([]))
+print(bool([1]))
+
+# in operator
+# checks if the left value is a member of the right value. Works on sequences (strings, lists, etc )
+print("a" in "bat")
+print("A" in "bat")
+print("onto" in "Toronto")
+print("ontos" in "Toronto")
