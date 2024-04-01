2bc91a43b97230c3e80b8b5cf359d56b5cc494e5
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-04-01 11:14:08 -0400

    python conditionals

---
+++ python_crash_course/conditionals/conditionals.py
@@ -0,0 +1,17 @@
+# Conditionals 
+age = int(input("How old are you? "))
+
+if age >= 21:
+    print("Come on in!")
+else: 
+    print("Go home kid!")
+
+color = "blue"
+
+# Using elif (else if)
+if color == "green":
+    print("Beginner")
+elif color == "blue":
+    print("Intermediate")
+elif color == "black":
+    print("Advanced")
