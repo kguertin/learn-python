15d5b9fb746bfe3b8e80399505d2305d36b055f3
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-04-10 10:28:34 -0400

    python function basics

---
+++ python_crash_course/functions/functions_basics.py
@@ -0,0 +1,23 @@
+# Basics of python functions
+
+# Need to define the function before executing it.
+def laugh():
+    print("Ha!" * 20)
+
+laugh()
+
+def laugh_by_number(intensity):
+    print("Ha!" * intensity)
+
+laugh_by_number(1)
+laugh_by_number(5)
+laugh_by_number(10)
+
+def divide(x, y):
+    print(x / y)
+
+divide(1,2)
+divide(45,6.4)
+divide(100, 33.3)
+divide(4, 100)
+divide(100, 4)
