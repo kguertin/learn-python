b595388741185e43b9a8aecec89d7540ef6951f4
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-04-11 10:16:42 -0400

    keyword/name arguments

--- python_crash_course/functions/functions_basics.py
+++ python_crash_course/functions/functions_basics.py
@@ -45,8 +45,22 @@ print(url)
 url = slugify("       HELLO world I LOVE YOU         ", "X")
 print(url)
 
-# Default value must come after parameters without default.
+# Default value must come after parameters without default
 def greet( person, msg = "Hi",):
     print(f"{msg}, {person}!")
 
 greet("Kevin")
+
+# Keyword / Named arguments
+def get_total(price, qty=1, tax=0.02, discount=0):
+    subtotal = price * qty * (1 - discount)
+    print(subtotal * (1 + tax))
+
+get_total(4.99, 5, 0.015, 0.2) 
+# ORder doesn't matter with key word arguments, can no longer use positional args when using a single keyword arg.
+get_total(price=9.75, qty=5, discount=0.5, tax=0.01)
+get_total(9.75, 5, 0.01, 0.5)
+get_total(8.99)
+get_total(8.99, tax=0.025)
+
+
