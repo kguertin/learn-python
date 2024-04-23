782bdedce95393040377e902e225b3f88aacfd10
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-04-23 12:41:19 -0400

    common loop patterns

--- python_crash_course/lists/lists.py
+++ python_crash_course/lists/lists.py
@@ -89,3 +89,28 @@ idx = 0
 while idx < len(num_list):
     print(f"The index is {idx}, the value is {num_list[idx]}")
     idx += 1
+
+# Lists and loop patterns
+lando_2021_results = [4,3,5,8,3,5,5,5,3,4,14,10,2,7,7,8,10,10,9,10,7]
+
+total = 0
+for result in lando_2021_results:
+    total += result
+
+avg = total / len(lando_2021_results)
+print(avg)
+
+def average(nums):
+    total = 0
+    for num in nums:
+        total += num
+    return total / len(nums)
+
+print(average(lando_2021_results))
+
+min = lando_2021_results[0]
+for result in lando_2021_results:
+    if result < min:
+        min = result
+        
+print(min)
