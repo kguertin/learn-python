7035eaad611006fbb04f6d2d809c4fcc2e6561ea
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-04-30 10:35:51 -0400

    todo refactor

--- python_crash_course/exercises/todo.py
+++ python_crash_course/exercises/todo.py
@@ -1,38 +1,37 @@
 # Todo Script
-todo_list = []
-completed = []
-
 print("""
  |_   _|__   __| | ___  ___ 
    | |/ _ \ / _` |/ _ \/ __|
    | | (_) | (_| | (_) \__ \\
    |_|\___/ \__,_|\___/|___/
 """)
+todo_list = []
+completed = []
 
-quit = False
-while quit == False:
+while True:
+    for i in range(len(todo_list)):
+        print(f"{i + 1}) {todo_list[i]}")
     print("*" * 25)
     command = input("Enter a command. Type 'h' for help:\n> ")
 
     if command == "q":
-        print(f"Today you completed {len(completed)} todos:")
-        for item in completed: 
-            print(f"* {item}")
-        quit = True
+        break
     elif command == "h":
         print("TODO LIST HELP")
         print("Type 'q' to quit")
         print("To add a todo to the list type it and hit enter")
         print("To complete a todo enter its number")
-    elif command.isdigit():
-        item = int(command)
-        if item <= len(todo_list):
-            completed.append(todo_list.pop(item - 1))
+    elif command.isnumeric():
+        idx = int(command) - 1
+        if idx >= len(todo_list):
+            print("There is no todo with that number")
+        else:
+            completed.append(todo_list.pop(idx))
     else:
         if len(command.strip()) > 0:
             todo_list.append(command)
-
-    idx = 0
-    while idx < len(todo_list):
-        print(f"{idx + 1}) {todo_list[idx]}")
-        idx += 1
+            
+if len(completed) > 0:
+    print(f"Today you completed {len(completed)} todos:")
+    for item in completed: 
+        print(f"* {item}")
