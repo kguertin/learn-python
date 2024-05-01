5ef28486b657a85fc622cbca67492d04b19b4d86
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-05-01 11:01:58 -0400

    accessing values safely

--- python_crash_course/dictionaries/dictionaries.py
+++ python_crash_course/dictionaries/dictionaries.py
@@ -8,3 +8,19 @@ print(movie)
 
 # Accessing Dictionary Values 
 print(movie["title"])
+
+# Updating Values
+movie["title"] = "King Kong"
+movie["imdb_score"] -= 0.5
+print(movie)
+
+# Adding new key / value pairs
+movie["rating"] = "R"
+print(movie)
+
+# in operator - Only checks the keys 
+print("title" in movie)
+
+# get() - pass a key, returns value or none
+print(movie.get("title"))
+print(movie.get("cast"))
