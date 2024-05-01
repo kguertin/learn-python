f0736f2331a90db341eea4a35427e043570f3479
Kevin Guertin <kevin.m.guertin@gmail.com>
2024-05-01 10:46:22 -0400

    dictionary basics

---
+++ python_crash_course/dictionaries/dictionaries.py
@@ -0,0 +1,10 @@
+# Dictionaries
+empty_dict = {}
+print(type(empty_dict))
+
+# Key / Value pairs 
+movie = {"title": "Amadeus", "imdb_score": 8.3}
+print(movie)
+
+# Accessing Dictionary Values 
+print(movie["title"])
